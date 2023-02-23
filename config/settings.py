b0d165ea1b975f9559873b38d8938d76c60791fd
Torajonov <torajonov3112@gmail.com>
2023-02-23 16:11:39 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR / "static")
+    if not DEBUG:
+        STATIC_ROOT = ''
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static/'),
+    ]
 
-
-STATICFILES_DIRS = (
-     os.path.join(BASE_DIR / "staticfiles"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR / "staticfiles"),)
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
